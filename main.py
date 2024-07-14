9ca5e0c7a4280d358637295d8020cf9bde4614fa
pineapple789 <pavel_turaev2828@mail.ru>
2024-07-14 13:05:37 +0800

    add edit commands

--- main.py
+++ main.py
@@ -85,42 +85,45 @@ def welcomePoint(msg: telebot.types.Message):
 
 
 # -reg client functions
-def regClientGen(msg: telebot.types.Message):
-    reply = bot.send_message(msg.chat.id, 'ask about his name')
-    msg: telebot.types.Message = yield reply, False
-    clientName = msg.text
+def regClientGen(msg: telebot.types.Message, client=(None, None, None)):
+    if client[0] is None:
+        reply = bot.send_message(msg.chat.id, 'ask about his name')
+        msg: telebot.types.Message = yield reply, False
+        client = (msg.text, client[1], client[2])
 
-    cityKeyboard = telebot.types.ReplyKeyboardMarkup()  # prepare keybaard
-    cityList = dbFunc.getCityList()
-    for city in cityList:
-        cityKeyboard.add(city)
+    if client[1] is None:
+        cityKeyboard = telebot.types.ReplyKeyboardMarkup()  # prepare keybaard
+        cityList = dbFunc.getCityList()
+        for city in cityList:
+            cityKeyboard.add(city)
 
-    reply = bot.send_message(msg.chat.id, 'ask about city, say about /rename', reply_markup=cityKeyboard)
-    msg = yield reply, False
-    clientCity = msg.text
-    while clientCity not in cityList:  # wait for correct city
-        reply = bot.send_message(msg.chat.id, 'incorrect city')
+        reply = bot.send_message(msg.chat.id, 'ask about city, say about /rename', reply_markup=cityKeyboard)
         msg = yield reply, False
-        clientCity = msg.text
+        while msg.text not in cityList:  # wait for correct city
+            reply = bot.send_message(msg.chat.id, 'incorrect city')
+            msg = yield reply, False
 
-    pointKeyboard = telebot.types.ReplyKeyboardMarkup()  # prepare keyboard
-    pointList = dbFunc.getPointsByCity(clientCity)
-    for point in pointList:
-        pointKeyboard.add(point[2])
+        client = (client[0], msg.text, client[2])
+    clientCity = client[1]
 
-    reply = bot.send_message(msg.chat.id, 'ask about point, say about /change_point', reply_markup=pointKeyboard)
-    msg = yield reply, False
-    clientBind = msg.text  # client is binding to point
-    while clientBind not in map(lambda el: el[2], pointList):  # wait for correct point
-        reply = bot.send_message(msg.chat.id, 'incorrect point')
+    if client[2] is None:
+        pointKeyboard = telebot.types.ReplyKeyboardMarkup()  # prepare keyboard
+        pointList = dbFunc.getPointsByCity(clientCity)
+        for point in pointList:
+            pointKeyboard.add(point[2])
+
+        reply = bot.send_message(msg.chat.id, 'ask about point, say about /change_point', reply_markup=pointKeyboard)
         msg = yield reply, False
-        clientBind = msg.text
+        while msg.text not in map(lambda el: el[2], pointList):  # wait for correct point
+            reply = bot.send_message(msg.chat.id, 'incorrect point')
+            msg = yield reply, False
 
-    # find point by point name
-    clientBindId = next(pTuple for pTuple in pointList if pTuple[2] == clientBind and pTuple[1] == clientCity)[0]
+        # find point by point name
+        clientBindId = next(pTuple for pTuple in pointList if pTuple[2] == msg.text and pTuple[1] == clientCity)[0]
+        client = (client[0], client[1], clientBindId)
 
-    botLogger.debug('saving client:' + str((msg.from_user.id, clientName, clientCity, clientBindId)))
-    dbFunc.addNewClient(msg.from_user.id, clientName, clientCity, clientBindId)
+    botLogger.debug('saving client:' + str((msg.from_user.id, client)))
+    dbFunc.addNewClient(msg.from_user.id, *client)
 
     reply = bot.send_message(msg.chat.id, 'data saved, say about /change_point',
                              reply_markup=telebot.types.ReplyKeyboardRemove())
@@ -144,7 +147,7 @@ def welcomeClient(msg: telebot.types.Message):
 
 
 # -reg consultant
-@bot.message_handler(commands=['set_name'], func=lambda msg: dbFunc.getPointById(msg.chat.id) is not None)
+@bot.message_handler(commands=['set_name', 'rename'], func=lambda msg: dbFunc.getPointById(msg.chat.id) is not None)
 def setNameConsultant(msg: telebot.types.Message):
     name = msg.text
     name = name[name.find(' ') + 1:]
@@ -152,10 +155,37 @@ def setNameConsultant(msg: telebot.types.Message):
     dbFunc.addNewConsultant(msg.from_user.id, name)
 
 
+# -edit functions
+#   -client side
+#       -change point
+@bot.message_handler(commands=['change_point'], func=lambda message: message.chat.type == 'private')
+def changeClientPoint(msg: telebot.types.Message):
+    client = dbFunc.getClientById(msg.chat.id)
+    if client is None:
+        bot.send_message(msg.chat.id, 'please enter /start for register')
+        return
 
-# TODO realise changing handers
+    replaceProc = regClientGen(msg, (client[1], None, None))
+    reply, stopReg = next(replaceProc)
+    if not stopReg:
+        bot.register_next_step_handler(reply, regClient, replaceProc)
+
+
+#       -change name
+@bot.message_handler(commands=['set_name', 'rename'], func=lambda message: message.chat.type == 'private')
+def changeClientName(msg: telebot.types.Message):
+    client = dbFunc.getClientById(msg.chat.id)
+    if client is None:
+        bot.send_message(msg.chat.id, 'please enter /start for register')
+        return
+
+    renameProc = regClientGen(msg, (None, client[2], client[3]))
+    reply, stopReg = next(renameProc)
+    if not stopReg:
+        bot.register_next_step_handler(reply, regClient, renameProc)
+
+
 # TODO realise delete group handler
-# TODO realise redirect handler
 
 # functions for communication
 #   -handle repeat message from telegram
@@ -233,6 +263,7 @@ def redirectClientGen(msg: telebot.types.Message, client):
     reply = bot.reply_to(postMsg, 'say about /cancel, ask about city\ncities:\n' + '\n'.join(cityList))
 
     pointList = []
+    newCity = ''
     while len(pointList) == 0:  # ! not vertified
         msg = yield reply, False  # waiting for city
 
